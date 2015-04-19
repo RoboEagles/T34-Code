74a2e9a605c20703c7538a5843b00be8fb98dd3f
Jaden Bottemiller <jadenbottemiller@gmail.com>
2015-04-19 12:13:20 -0700

    Added Measurement Items

--- T34/src/org/usfirst/frc4579/T34/commands/Accumulate.java
+++ T34/src/org/usfirst/frc4579/T34/commands/Accumulate.java
@@ -31,10 +31,12 @@ public class  Accumulate extends Command {
 
     // Called just before this Command runs the first time
     protected void initialize() {
+    	Robot.measurement.calibrate();
     }
 
     // Called repeatedly when this Command is scheduled to run
     protected void execute() {
+    	Robot.measurement.accumulate();
     }
 
     // Make this return true when this Command no longer needs to run execute()
@@ -44,10 +46,13 @@ public class  Accumulate extends Command {
 
     // Called once after isFinished returns true
     protected void end() {
+    	Robot.measurement.setInvalid();
+    	System.out.println("Measurement has ended");
     }
 
     // Called when another command which requires one or more of the same
     // subsystems is scheduled to run
     protected void interrupted() {
+    	end();
     }
 }
