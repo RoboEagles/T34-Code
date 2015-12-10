cba29336a21d33eb4a7957b949fba0e9c11abf07
Jaden Bottemiller <jadenbottemiller@gmail.com>
2015-12-09 18:35:26 -0800

    Refactoring things, and adding essentials

---
+++ T34/src/com/RoboEagles4579/math/Vector3d.java
@@ -0,0 +1,22 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.RoboEagles4579.math;
+
+/**
+ *
+ * @author bstevens
+ */
+public class Vector3d {
+    
+    public double X, Y, Z;
+    
+    public Vector3d(double x, double y, double z) {
+        this.X = x;
+        this.Y = y;
+        this.Z = z;
+    }
+    
+}
